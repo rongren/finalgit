0e3e6501bf56c9f74e4bb1cb617f3d561e277757
gihyeon <gihyeon6394@gmail.com>
2019-09-10 17:00:48 +0900

    gihyeonSecondDay

--- 04_FinalGitTest/src/main/java/com/kh/spring/KimGiHyeon.java
+++ 04_FinalGitTest/src/main/java/com/kh/spring/KimGiHyeon.java
@@ -3,8 +3,7 @@ package com.kh.spring;
 public class KimGiHyeon {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("dsfjdshdsfjdfsjdfsjfds");
 	}
 
 }
