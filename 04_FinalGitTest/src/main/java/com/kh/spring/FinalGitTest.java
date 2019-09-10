56636351879b0c742fe559428ca211e3d4bf2198
gihyeon <gihyeon6394@gmail.com>
2019-09-10 16:38:49 +0900

    gihyeonStart

--- 04_FinalGitTest/src/main/java/com/kh/spring/FinalGitTest.java
+++ 04_FinalGitTest/src/main/java/com/kh/spring/FinalGitTest.java
@@ -4,5 +4,6 @@ public class FinalGitTest {
 
 	public void test() {
 		System.out.println("팀장이 프로젝트 시작함.");
+		System.out.println("김기현 작업시작했어요!!");
 	}
 }
---
+++ 04_FinalGitTest/src/main/java/com/kh/spring/KimGiHyeon.java
@@ -0,0 +1,10 @@
+package com.kh.spring;
+
+public class KimGiHyeon {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
